e15be5b5e4399a0255b945de6aaff2607056f2e9
Taylor Wilkinson <taylorwlearns@gmail.com>
2019-05-14 10:22:50 -0500

    added media queries to register form

--- views/RegisterView.php
+++ views/RegisterView.php
@@ -1,25 +1,25 @@
 <?php include 'Header.php'; ?>
      <main class="login">
           <div class="container">
-               <div class="login-form-wrap">
-                    <form class="login-form" action="DoRegister.php" method="POST">
-                         <h2 class="pad-bottom text-center">Sign Up!</h2>
-                         <div class="form-field">
-                              <label for="username">Email</label>
-                              <input type="text" name="email" id="email" placeholder="ex: john@something.com" />
-                         </div>
-                         <div class="form-field">
-                              <label for="username">Username</label>
-                              <input type="text" name="username" id="username" placeholder="ex: iluvrecipes" />
-                         </div>
-                         <div class="form-field">
-                              <label for="password">Password</label>
-                              <input type="password" name="password" id="password" placeholder="password" />
-                         </div>
-                         <div class="text-center">
-                              <input type="submit" value="Sign Up" class="btn" />
-                         </div>
-                         <p class="message">Already registered? <a href="login.php">Login</a></p>
+               <div class="register-form-wrap">
+                    <form class="register-form" action="DoRegister.php" method="POST">
+                        <h2 class="pad-bottom text-center">Sign Up!</h2>
+                        <div class="form-field">
+                             <label for="username">Email</label>
+                             <input type="text" name="email" id="email" placeholder="ex: john@something.com" />
+                        </div>
+                        <div class="form-field">
+                             <label for="username">Username</label>
+                             <input type="text" name="username" id="username" placeholder="ex: iluvrecipes" />
+                        </div>
+                        <div class="form-field">
+                             <label for="password">Password</label>
+                             <input type="password" name="password" id="password" placeholder="password" />
+                        </div>
+                        <div class="text-center">
+                             <input type="submit" value="Sign Up" class="btn" />
+                        </div>
+                        <p class="message">Already registered? <a href="login.php">Login</a></p>
                     </form>
                </div>
           </div>
