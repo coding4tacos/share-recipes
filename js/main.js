676961ae0d338253ae166c6ff97f37b97d1dc921
Taylor Wilkinson <taylorwlearns@gmail.com>
2019-05-18 15:51:13 -0500

    improved form validation by adding settimeout on error messages.

--- js/main.js
+++ js/main.js
@@ -10,28 +10,50 @@ const registerPage = document.querySelector('main.register'),
              usernameInput = document.getElementById('username'),
              passwordInput = document.getElementById('password'),
              errorMessage = document.querySelector('.error-message');
+             
+        
                           
              
         function validate(e) {
             if(emailInput.value === '' || !emailInput.value.includes('@') || !emailInput.value.includes('.') || emailInput.value.length < 5) {
                 e.preventDefault();
                 emailInput.style.border = '1px solid red';
-                
                 errorMessage.textContent = '*Please add a valid email.';
+                // reset after 2 seconds
+                setTimeout(function(){ 
+                emailInput.style.border = '2px solid rgba(57, 126, 56, .5)';
+
+                errorMessage.textContent = null;
+                }, 2500);
             
                 
             } else if(usernameInput.value === '' || usernameInput.value.length < 2) {
                 e.preventDefault();
+                
+                
                 usernameInput.style.border = '1px solid red';
                 
                 errorMessage.textContent = '*Usernames must be at least 3 characters.';
+                    // reset after 2 seconds
+                    setTimeout(function(){ 
+                    usernameInput.style.border = '2px solid rgba(57, 126, 56, .5)';
 
+                    errorMessage.textContent = null;
+                }, 2500);
                 
             } else if(passwordInput.value === '' || passwordInput.value.length < 5) {
                 e.preventDefault();
                 passwordInput.style.border = '1px solid red';
                 
                 errorMessage.textContent = '*Passwords must be at least 5 characters.';
+                
+                // reset after 2 seconds
+                    setTimeout(function(){ 
+                    passwordInput.style.border = '2px solid rgba(57, 126, 56, .5)';
+
+                    errorMessage.textContent = null;
+                }, 2500);
+                
             }     
         }
         registerForm.addEventListener("submit", validate);
@@ -49,9 +71,20 @@ const registerPage = document.querySelector('main.register'),
                  if(usernameInput.value === '') {
                     e.preventDefault();
                     usernameInput.style.border = '1px solid red';
+                    
+                    // reset after 2 seconds
+                    setTimeout(function(){ 
+                    usernameInput.style.border = '2px solid rgba(57, 126, 56, .5)';
+                    }, 2500);
+                
                  } else if(passwordInput.value === '') {
                     e.preventDefault();
                     passwordInput.style.border = '1px solid red';
+                    
+                    // reset after 2 seconds
+                    setTimeout(function(){ 
+                    passwordInput.style.border = '2px solid rgba(57, 126, 56, .5)';
+                    }, 2500);
                  }
              }
              loginForm.addEventListener("submit", validate);  
