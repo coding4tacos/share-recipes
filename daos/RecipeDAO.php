8876068503d28d1b794a0a3785ea40a12d54f41b
Taylor Wilkinson <taylorwlearns@gmail.com>
2019-05-25 11:10:12 -0500

    working on prepared statements for recipe DAO

--- daos/RecipeDAO.php
+++ daos/RecipeDAO.php
@@ -20,10 +20,21 @@ class RecipeDAO {
     public function create($recipe) {
         $sql="insert into recipes ( name, description, ingredients, instructions, img) values ( '{$recipe->getName()}', '{$recipe->getDescription()}', '{$recipe->getIngredients()}', '{$recipe->getInstructions()}', '{$recipe->getImg()}')";
         
-        // $stmt = $mysqli->prepare("INSERT INTO recipes ( name, description, ingredients, instructions, img ) VALUES ( ?, ?, ?, ?, ?)");
+            // Create a template
+            // $sql = $mysqli->prepare("INSERT INTO recipes ( name, description, ingredients, instructions, img ) VALUES ( ?, ?, ?, ?, ?)");
+            // Createa prepared statement
+        // $stmt = mysqli_stmt_init($this->con);
+        // prepare the prepared statement
+        // if(!mysqli_stmt_prepare($stmt, $sql)) {
+        //      echo "SQL statement failed";
+        // }
+        
+        
        //   $stmt->bind_param("ssssa", '{$recipe->getName()}', '{$recipe->getDescription()}', '{$recipe->getIngredients()}', '{$recipe->getInstructions()}', '{$recipe->getImg()}');
         // $stmt->execute();
         
+        // https://www.youtube.com/watch?v=I4JYwRIjX6c
+         
         
         
         
