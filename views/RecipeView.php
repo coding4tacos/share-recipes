dff06fe5a0e8b265b9ba1c86b4d985d53f7e0c58
Taylor Wilkinson <taylorwlearns@gmail.com>
2019-05-31 16:06:05 -0500

    styles for the single review view are looking better

--- views/RecipeView.php
+++ views/RecipeView.php
@@ -13,20 +13,19 @@
         <div class="container">
             <div class="section-header">
                 <h2 class="text-center"><?php echo $data["recipes"]->getName(); ?></h2>
+                <p class="recipe-description text-center"><?php echo $data["recipes"]->getDescription(); ?></p>
             </div>
-            <div class="">
-                <img class="featured-recipe-img" src="./img/<?php echo $data["recipes"]->getImg(); ?>" alt="<?php echo $data["recipes"]->getName(); ?>">
-                <h4 class="text-center"><?php echo $data["recipes"]->getName(); ?></h4>
-            </div>
-            <h3>Description:</h3>
-                <p class="recipe-description"><?php echo $data["recipes"]->getDescription(); ?></p>
-                   
-                <h3>Ingredients:</h3>
-                <p class=""><?php echo $data["recipes"]->getIngredients(); ?></p>
-                <h3>Instructions:</h3>
-                <p class=""><?php echo $data["recipes"]->getInstructions(); ?></p>
+            <div class="flex single-recipe">
+                <div class="recipe-image"> 
+                    <img class="featured-recipe-img" src="./img/<?php echo $data["recipes"]->getImg(); ?>" alt="<?php echo $data["recipes"]->getName(); ?>">
                 </div>
-        
+                <div class="recipe-info">
+                    <h3>Ingredients:</h3>
+                    <p class=""><?php echo $data["recipes"]->getIngredients(); ?></p>
+                    <h3>Instructions:</h3>
+                    <p class=""><?php echo $data["recipes"]->getInstructions(); ?></p>
+                </div>
+                </div>
             <div class="add-recipe text-center">
                 <a href="Login.php">login</a> or <a href="Register.php">register</a> to add a new recipe
             </div>
